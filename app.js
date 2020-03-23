4e9b3d0a7a4bf02cac9db15e88c94b6758c26327
Andrew Durrant <andrew.durrant75@gmail.com>
2020-03-23 15:32:50 -0600

    finish walkthrough

--- app.js
+++ app.js
@@ -12,6 +12,45 @@ app.get('/burgers', (req, res) => {
   res.send('We have juicy cheese burgers!');
 });
 
+app.get('/echo', (req, res) => {
+  const responseText = `Here are some details of your request:
+    IP: ${req.ip}
+    Params: ${req.params}
+    Path: ${req.path}
+  `;
+  res.send(responseText);
+});
+
+app.get('/queryViewer', (req, res) => {
+  console.log(req.query);
+  res.end(); //do not send any data back to the client
+});
+
+app.get('/greetings', (req, res) => {
+  //1. get values from the request
+  const name = req.query.name;
+  const race = req.query.race;
+
+  //2. validate the values
+  if(!name) {
+    //3. name was not provided
+    return res.status(400).send('Please provide a name');
+  }
+
+  if(!race) {
+    //3. race was not provided
+    return res.status(400).send('Please provide a race');
+  }
+
+  //4. and 5. both name and race are valid so do the processing.
+  const greeting = `Greetings ${name} the ${race}, welcome to our kingdom.`;
+
+  //6. send the response 
+  res.send(greeting);
+});
+
+
+
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
 });
